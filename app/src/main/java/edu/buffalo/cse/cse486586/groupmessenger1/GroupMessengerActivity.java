f1c95973fb41aec69cae925cc2587823c9d82c1a
Firnaz <firnazluztian@gmail.com>
2017-02-22 15:51:04 -0500

    Initialized button and edittext, declare server task and client task

--- app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
@@ -1,23 +1,59 @@
 package edu.buffalo.cse.cse486586.groupmessenger1;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 /**
  * GroupMessengerActivity is the main Activity for the assignment.
- * 
+ *
  * @author stevko
  *
  */
 public class GroupMessengerActivity extends Activity {
 
+    // Reusing code from p1
+    static final String TAG = GroupMessengerActivity.class.getSimpleName();
+    static final String REMOTE_PORT0 = "11108";
+    static final String REMOTE_PORT1 = "11112";
+    static final String REMOTE_PORT2 = "11116";
+    static final String REMOTE_PORT3 = "11120";
+    static final String REMOTE_PORT4 = "11124";
+    static final int SERVER_PORT = 10000;
+
+    EditText editText;
+    Button button4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_messenger);
+
+        // Reusing code from p1
+        TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            Log.e(TAG, "Can't create a ServerSocket");
+            return;
+        }
 
         /*
          * TODO: Use the TextView to display your messages. Though there is no grading component
@@ -25,20 +61,55 @@ public class GroupMessengerActivity extends Activity {
          */
         TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
-        
+
         /*
          * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
          * OnPTestClickListener demonstrates how to access a ContentProvider.
          */
         findViewById(R.id.button1).setOnClickListener(
                 new OnPTestClickListener(tv, getContentResolver()));
-        
+
         /*
          * TODO: You need to register and implement an OnClickListener for the "Send" button.
          * In your implementation you need to get the message from the input box (EditText)
          * and send it to other AVDs.
          */
+
+        editText = (EditText)findViewById(R.id.editText1);
+        button4 = (Button)findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = editText.getText().toString() + "\n";
+                editText.setText(""); // This is one way to reset the input box.
+                TextView localTextView = (TextView) findViewById(R.id.textView1);
+                localTextView.append("\t" + msg); // This is one way to display a string.
+                TextView remoteTextView = (TextView) findViewById(R.id.textView1);
+                remoteTextView.append("\n");
+
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
+            }
+        });
+
     }
+
+    // Reusing code from p1
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            return null;
+        }
+    }
+
+    // Reusing code from p1
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+     @Override
+     protected Void doInBackground(String... params) {
+         return null;
+     }
+ }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
